268878b327f7b679250891443a8a77dd761f1817
Elliot <ewhite29@gmail.com>
2017-04-26 14:34:34 -0500

    Added python files and change HTML to point to python scripts

---
+++ SchedMe/addevent.py
@@ -0,0 +1,24 @@
+#!"C:\Users\Rusty Shackleford\AppData\Local\Programs\Python\Python36-32\python.exe"
+import cgi
+
+form = cgi.FieldStorage() 
+name = form.getvalue("Event Name")
+time  = form.getvalue("Start Time")
+group = form.getvalue("Group Name")
+descrip = form.getvalue("Description...")
+location = form.getvalue("Event Location")
+
+events = open('events.txt', 'w')
+
+events.write(name)
+events.write('\n')
+events.write(time)
+events.write('\n')
+events.write(group)
+events.write('\n')
+events.write(descrip)
+events.write('\n')
+events.write(location)
+events.write('\n')
+
+users.close()
---
+++ SchedMe/adduser.py
@@ -0,0 +1,24 @@
+#!"C:\Users\Rusty Shackleford\AppData\Local\Programs\Python\Python36-32\python.exe"
+import cgi
+
+form = cgi.FieldStorage() 
+firstname = form.getvalue('firstname')
+lastname  = form.getvalue('lastname')
+email = form.getvalue('your.email@here.com')
+password = form.getvalue('password')
+repass = form.getvalue('re-enter password')
+
+users = open('users.txt', 'w')
+
+users.write(firstname)
+users.write('\n')
+users.write(lastname)
+users.write('\n')
+users.write(email)
+users.write('\n')
+users.write(password)
+users.write('\n')
+users.write(repass)
+users.write('\n')
+
+users.close()
